964eca530a7b4dcc5cd7267c900646e0f55d267e
CandraJengger <cakjengger@gmail.com>
2021-08-13 22:45:49 +0700

    Create Button variants

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import variants from './button.variant';
 
-const Button = ({ text, ...props }) => {
+const Button = ({ text, variant = 'primary', ...props }) => {
+  const style = variants[variant];
+
   return (
-    <button
-      {...props}
-      className="py-4 px-12 w-full rounded-2xl bg-primary hover:bg-green-700 duration-300 text-white"
-    >
+    <button {...props} className={style}>
       {text}
     </button>
   );
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Button } from '../../components/atoms';
+import { PlainLayout } from '../../components/organisms';
 
 const SignInPage = () => {
-  return <div>SignIn</div>;
+  return (
+    <PlainLayout>
+      <div className="flex flex-col justify-center items-center">
+        <Button text="Masuk" />
+      </div>
+    </PlainLayout>
+  );
 };
 
 export default SignInPage;
